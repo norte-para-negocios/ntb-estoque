ecc8ea4330c2b3559f4e7c89699dc095cd337167
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2025-06-25 21:46:13 -0300

    <feat>(Notasfiscais): Adicionado filtro por: produto, status, tipo , Closes #8

--- resources/views/notafiscal/index.blade.php
+++ resources/views/notafiscal/index.blade.php
@@ -36,18 +36,55 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-2">
+                                <div class="col-md-1">
                                     <div class="mb-3">
-                                        <label for="num_nfe" class="form-label">Número NFe</label>
-                                        <input type="text" id="num_nfe" name="num_nfe" placeholder="Nº NFe"
+                                        <label for="num_nfe" class="form-label">Nº NFe</label>
+                                        <input type="text" id="num_nfe" name="num_nfe"
                                             class="form-control" value="{{ request('num_nfe', $num_nfe ?? '') }}">
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-2">
                                     <div class="mb-3">
-                                        <label for="num_nfe" class="form-label">Fornecedor</label>
-                                        <input type="text" id="fornecedor" name="fornecedor" placeholder="Coca-cola"
+                                        <label for="fornecedor" class="form-label">Fornecedor</label>
+                                        <input type="text" id="fornecedor" name="fornecedor"
                                             class="form-control" value="{{ request('fornecedor', $fornecedor ?? '') }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="mb-3">
+                                        <label for="produto" class="form-label">Produto</label>
+                                        <input type="text" id="produto" name="produto"
+                                            class="form-control" value="{{ request('produto', $produto ?? '') }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="mb-3">
+                                        <label for="tipo" class="form-label">Tipo</label>
+                                        <select id="tipo" name="tipo" class="form-control">
+                                            <option value="">Todos os tipos</option>
+                                            <option value="00" {{ request('tipo', $tipo ?? '') == '00' ? 'selected' : '' }}>Mercadoria para Revenda</option>
+                                            <option value="01" {{ request('tipo', $tipo ?? '') == '01' ? 'selected' : '' }}>Matéria Prima</option>
+                                            <option value="02" {{ request('tipo', $tipo ?? '') == '02' ? 'selected' : '' }}>Embalagem</option>
+                                            <option value="03" {{ request('tipo', $tipo ?? '') == '03' ? 'selected' : '' }}>Produto em Processo</option>
+                                            <option value="04" {{ request('tipo', $tipo ?? '') == '04' ? 'selected' : '' }}>Produto Acabado</option>
+                                            <option value="05" {{ request('tipo', $tipo ?? '') == '05' ? 'selected' : '' }}>Subproduto</option>
+                                            <option value="06" {{ request('tipo', $tipo ?? '') == '06' ? 'selected' : '' }}>Produto Intermediário</option>
+                                            <option value="07" {{ request('tipo', $tipo ?? '') == '07' ? 'selected' : '' }}>Material de Uso e Consumo</option>
+                                            <option value="08" {{ request('tipo', $tipo ?? '') == '08' ? 'selected' : '' }}>Ativo Imobilizado</option>
+                                            <option value="09" {{ request('tipo', $tipo ?? '') == '09' ? 'selected' : '' }}>Serviços</option>
+                                            <option value="10" {{ request('tipo', $tipo ?? '') == '10' ? 'selected' : '' }}>Outros Insumos</option>
+                                            <option value="99" {{ request('tipo', $tipo ?? '') == '99' ? 'selected' : '' }}>Outras</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="mb-3">
+                                        <label for="status" class="form-label">Status</label>
+                                        <select id="status" name="status" class="form-control">
+                                            <option value="">Todos</option>
+                                            <option value="P" {{ request('status', $status ?? '') == 'P' ? 'selected' : '' }}>Pendente</option>
+                                            <option value="C" {{ request('status', $status ?? '') == 'C' ? 'selected' : '' }}>Concluído</option>
+                                        </select>
                                     </div>
                                 </div>
 
