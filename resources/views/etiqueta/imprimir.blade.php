90f055926096fdf641dc23baab1537e6bf5a32b1
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2025-07-17 16:22:17 -0300

    Ajustado css dos campos da etiqueta

--- resources/views/etiqueta/imprimir.blade.php
+++ resources/views/etiqueta/imprimir.blade.php
@@ -48,56 +48,56 @@
             <table aria-hidden="true">
                 <tbody>
                     <tr>
-                        <td colspan="2" style="margin:0; padding: 0;">
+                        <td colspan="2" style="margin:0 !important; padding: 0 !important;">
                             <div class="product">
                                 {{ substr($etiqueta['descricao'], 0, 38) }}
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <td style="width: 30%; padding: 0;">
-                            <p style="margin-bottom: 0; padding-bottom: 0;">
+                        <td style="width: 30% !important; padding: 0 !important;">
+                            <p style="margin-bottom: 0 !important; padding-bottom: 0 !important;">
                                 {!! QrCode::size(102)->generate($etiqueta['codigo_produto']) !!}
                             </p>
-                            <p style="font-size: 8pt; padding: 0; margin: 0; line-height: 8pt;">
+                            <p style="font-size: 8pt !important; padding: 0 !important; margin: 0 !important; line-height: 8pt !important;">
                                 {{ substr(trim($etiqueta['codigo_produto']), 0, 16) }}
                             </p>
                         </td>
                         <td style="width: 70%;">
                             @if ($etiqueta['fornecedor'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     {{ substr(trim($etiqueta['fornecedor']), 0, 25) }}
                                 </span><br>
                             @endif
                             @if ($etiqueta['nfe'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     NF: {{ substr(trim($etiqueta['nfe']), 0, 21) }}
                                 </span><br>
                             @endif
                             @if ($etiqueta['lote'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     Lote: {{ substr(trim($etiqueta['lote']), 0, 19) }}
                                 </span><br>
                             @endif
                             @if ($etiqueta['quantidade'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     Quant.: {{ substr(trim($etiqueta['quantidade']), 0, 17) }}
                                 </span><br>
                             @endif
                             @if ($etiqueta['produzido'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     Embalado: {{ substr(trim($etiqueta['produzido']), 0, 15) }}
                                 </span><br>
                             @endif
                             @if ($etiqueta['validade'] !== '')
-                                <span style="margin-bottom: 2mm;">
+                                <span style="margin-bottom: 2mm !important;">
                                     Validade: {{ substr(trim($etiqueta['validade']), 0, 15) }}
                                 </span><br>
                             @endif
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="2" style="vertical-align: bottom;">
+                        <td colspan="2" style="vertical-align: bottom !important;">
                             <small>CNPJ:{{ trim(Auth::user()->loja->cnpj) ?? '-' }}</small>
                         </td>
                     </tr>
