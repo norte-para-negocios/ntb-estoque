1c8402691d2dbd53bfc7b5e78bb473b62b98f239
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2025-08-22 00:45:19 -0300

    Add validacao dos itens da nf

--- app/Services/NotaFiscalService.php
+++ app/Services/NotaFiscalService.php
@@ -199,7 +199,7 @@ class NotaFiscalService
             if (count($batchItems) > 0) {
                 NotaFiscalItem::upsert(
                     $batchItems,
-                    ['loja_id', 'nota_fiscal_id', 'n_id_receb', 'n_id_item', 'n_id_produto'],
+                    ['loja_id', 'nota_fiscal_id', 'n_id_receb', 'c_codigo_produto'],
                     array_keys($batchItems[0])
                 );
             }
