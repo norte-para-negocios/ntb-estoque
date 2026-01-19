b1b17b7113a50dce222942bb16a2506891dde90b
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2026-01-19 20:38:01 -0300

    Add validacao de itens inativos

--- app/Http/Controllers/Transferencia/TransferenciaController.php
+++ app/Http/Controllers/Transferencia/TransferenciaController.php
@@ -193,6 +193,11 @@ class TransferenciaController extends Controller
 
         // Consulta simples com filtro e limite
         return Produto::where('loja_id', auth()->user()->current_loja_id)
+            ->where('full_object', '!=', null)
+            ->where(function ($query) {
+                $query->where('full_object', 'like', '%"inativo":"N"%')
+                    ->orWhereNull('full_object');
+            })
             ->where(function ($query) use ($term) {
                 $query->where('descricao', 'LIKE', "%{$term}%")
                     ->orWhere('codigo', 'LIKE', "%{$term}%")
