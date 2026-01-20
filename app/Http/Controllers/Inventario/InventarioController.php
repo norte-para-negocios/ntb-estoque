8b200bfe65b7d3aa885efadd680aef656f01ed77
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2026-01-19 21:14:54 -0300

    Corrigido erro ao enviar movimentacao para omie

--- app/Http/Controllers/Inventario/InventarioController.php
+++ app/Http/Controllers/Inventario/InventarioController.php
@@ -154,17 +154,6 @@ class InventarioController extends Controller
                 'status' => ($posicaoEstoque?->n_cmc > 0) ? null : 'Sem CMC',
             ]);
 
-            $response = $this->createAjuste($item);
-            if ($response) {
-                $item->response = json_encode($response);
-                $item->codigo_status = $response->codigo_status;
-                $item->descricao_status = $response->descricao_status;
-                $item->id_movest = $response->id_movest;
-                $item->id_ajuste = $response->id_ajuste;
-                $item->status = 'Concluído';
-            }
-            $item->save();
-
             return response([
                 "key" => $item->id,
                 "id" => $produto->codigo,
