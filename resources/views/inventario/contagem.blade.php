822bc43024baa60382d7bb8f2a74288abd164472
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2026-02-11 12:06:22 -0300

    Ajustado padding da pesquisa

--- resources/views/inventario/contagem.blade.php
+++ resources/views/inventario/contagem.blade.php
@@ -46,7 +46,7 @@
         <div class="px-2">
             <div class="card card-body mb-3">
                 <div class="row">
-                    <div class="col-md-7 col-12 pb-4">
+                    <div class="col-md-7 col-12 pb-md-1 pb-4">
                         <select class="search form-control fw-semibold rounded-0"
                                 placeholder="Digite para buscar..."
                                 id="search">
