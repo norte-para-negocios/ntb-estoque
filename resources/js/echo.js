e8220c6b060ab2d5b52be0bf8b6d59845ce61d00
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2026-02-12 12:08:11 -0300

    Add configurações do reverb

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,12 +3,21 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const reverbConfig = window.reverbConfig ?? {};
+const config = {
+     key: reverbConfig.key ?? import.meta.env.VITE_REVERB_APP_KEY,
+     wsHost: reverbConfig.host ?? import.meta.env.VITE_REVERB_HOST,
+     wsPort: reverbConfig.port ?? import.meta.env.VITE_REVERB_PORT ?? 80,
+     wssPort: reverbConfig.port ?? import.meta.env.VITE_REVERB_PORT ?? 443,
+     scheme: reverbConfig.scheme ?? import.meta.env.VITE_REVERB_SCHEME ?? 'https',
+ };
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    key: config.key,
+    wsHost: config.wsHost,
+    wsPort: config.wsPort,
+    wssPort: config.wssPort,
+    forceTLS: config.scheme === 'https',
     enabledTransports: ['ws', 'wss'],
 });
