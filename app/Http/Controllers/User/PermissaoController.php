63f72e9816f79424c70887c79ed3e1c62cc1f7ba
Rodolfo Melo <rodolfo.ba.melo@gmail.com>
2026-04-12 12:26:02 -0300

    <bugfix>(User/Local): Add verificação para impedir que o registro de local seja duplicado no banco de dados

--- app/Http/Controllers/User/PermissaoController.php
+++ app/Http/Controllers/User/PermissaoController.php
@@ -10,7 +10,6 @@ use App\Models\Permissao;
 use App\Models\PermissaoUser;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class PermissaoController extends Controller
 {
@@ -22,11 +21,12 @@ class PermissaoController extends Controller
     public function attach(Request $request, User $user)
     {
         if (auth()->user()->perfil !== 'Admin') {
-            abort(403, "Você não é um usuário Administrador!");
+            abort(403, 'Você não é um usuário Administrador!');
         }
 
         try {
             $user->permissoes()->attach($request->get('permissao_id'), ['loja_id' => $request->get('loja_id')]);
+
             return response(['success', 'Registro realizado com sucesso!']);
         } catch (\Throwable $th) {
             return response(['error', $th->getMessage()]);
@@ -36,7 +36,7 @@ class PermissaoController extends Controller
     public function detach(Request $request, User $user, Loja $loja, Permissao $permissao)
     {
         if (auth()->user()->perfil !== 'Admin') {
-            abort(403, "Você não é um usuário Administrador!");
+            abort(403, 'Você não é um usuário Administrador!');
         }
 
         try {
@@ -44,6 +44,7 @@ class PermissaoController extends Controller
                 ->where('loja_id', $loja->id)
                 ->where('permissao_id', $permissao->id)
                 ->delete();
+
             return response(['success', 'Registro excluído com sucesso!']);
         } catch (\Throwable $th) {
             return response(['error', $th->getMessage()]);
@@ -53,18 +54,26 @@ class PermissaoController extends Controller
     public function attachLocal(Request $request, User $user)
     {
         if (auth()->user()->perfil !== 'Admin') {
-            abort(403, "Você não é um usuário Administrador!");
+            abort(403, 'Você não é um usuário Administrador!');
         }
 
         try {
-            $user->locais()->attach(
-                $request->get('local_id'),
-                [
-                    'loja_id' => $request->get('loja_id'),
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ]
-            );
+            $localUserExists = LocalUser::where('loja_id', $request->get('loja_id'))
+                ->where('user_id', $user->id)
+                ->where('local_id', $request->get('local_id'))
+                ->exists();
+
+            if (! $localUserExists) {
+                $user->locais()->attach(
+                    $request->get('local_id'),
+                    [
+                        'loja_id' => $request->get('loja_id'),
+                        'created_at' => date('Y-m-d H:i:s'),
+                        'updated_at' => date('Y-m-d H:i:s'),
+                    ]
+                );
+            }
+
             return response(['success', 'Registro realizado com sucesso!']);
         } catch (\Throwable $th) {
             return response(['error', $th->getMessage()]);
@@ -74,7 +83,7 @@ class PermissaoController extends Controller
     public function detachLocal(Request $request, User $user, Loja $loja, LocalEstoque $local)
     {
         if (auth()->user()->perfil !== 'Admin') {
-            abort(403, "Você não é um usuário Administrador!");
+            abort(403, 'Você não é um usuário Administrador!');
         }
 
         try {
@@ -82,6 +91,7 @@ class PermissaoController extends Controller
                 ->where('loja_id', $loja->id)
                 ->where('local_estoque_id', $local->id)
                 ->delete();
+
             return response(['success', 'Registro excluído com sucesso!']);
         } catch (\Throwable $th) {
             return response(['error', $th->getMessage()]);
